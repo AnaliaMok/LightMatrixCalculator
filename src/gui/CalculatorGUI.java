60501372e77dc132de7c5389f226a168738773a0
Analia Mok <axm2580@g.rit.edu>
2016-10-08 18:42:34 -0400

    Added CalculatorGUI to git

---
+++ src/gui/CalculatorGUI.java
@@ -0,0 +1,22 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * @author Analia Mok
+ */
+public class CalculatorGUI extends Application implements Observer{
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
