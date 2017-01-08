1e88eb558b21211abf701c6de13f4a2de1f3a1af
Analia Mok <axm2580@g.rit.edu>
2017-01-08 14:21:21 -0500

    Merged master and dimChangeV2 branch

--- src/gui/InputBox.java
+++ src/gui/InputBox.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
 import model.MatrixModel;
 
